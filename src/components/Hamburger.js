faf46bc76024f3bb8a9c510941900d91cb6c8f14
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-03-29 01:38:57 +0200

    Built the Menu animation with gsap and react

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,225 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
 
-const Hamburger = () => {
-    return <div className="menu">menu</div>
-}
+import dallas from "../images/dallas.webp";
+import austin from "../images/austin.webp";
+import beijing from "../images/beijing.webp";
+import newyork from "../images/newyork.webp";
+import sanfrancisco from "../images/sanfrancisco.webp";
+
+const cities = [
+  { name: "Dallas", image: dallas },
+  { name: "Austin", image: austin },
+  { name: "Beijing", image: beijing },
+  { name: "New York", image: newyork },
+  { name: "San francisco", image: sanfrancisco },
+];
+
+const Hamburger = ({ state }) => {
+  //UseRef (vars for our animated dom nodes)_
+  let menu = useRef(null);
+  let revealMenu = useRef(null);
+  let revealMenuBackground = useRef(null);
+  let cityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let info = useRef(null);
+
+  //UseEffect
+  useEffect(() => {
+    if (state.clicked === false) {
+      //close our menu
+      //menu.style.display = "none";
+      gsap.to([revealMenu, revealMenuBackground], {
+        duration: 0.8,
+        height: 0,
+        ease: "power3.inOut",
+        stagger: {
+          amount: 0.07,
+        },
+      });
+      gsap.to(menu, {
+        duration: 1,
+        css: {
+          display: "none",
+        },
+      });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      //Open our menu
+      //menu.style.display = "block";
+      gsap.to(menu, {
+        duration: 0,
+        css: {
+          display: "block",
+        },
+      });
+      gsap.to([revealMenuBackground, revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: "100%",
+      });
+      staggerReveal(revealMenuBackground, revealMenu);
+      fadeInUp(info);
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
+
+  const staggerReveal = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: 0.8,
+      height: 0,
+      transformOrigin: "right top",
+      skewY: 2,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.1,
+      },
+    });
+  };
+
+  const fadeInUp = (node1) => {
+    gsap.from(node1, {
+      y: 60,
+      duration: 1,
+      delay: 0.3,
+      opacity: 0,
+      ease: "power3.inOut",
+    });
+  };
+
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      duration: 0.8,
+      y: 100,
+      delay: 0.2,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.4,
+      },
+    });
+  };
+
+  const handleCity = (city) => {
+    gsap.to(cityBackground, {
+      duration: 0,
+      background: `url(${city}) center center`,
+    });
+    gsap.to(cityBackground, {
+      duration: 0.4,
+      opacity: 1,
+      ease: "power3.inOut",
+    });
+    gsap.from(cityBackground, {
+      duration: 0.4,
+      skewY: 2,
+      transformOrigin: "right top",
+    });
+  };
+
+  const handleCityRemove = () => {
+    gsap.to(cityBackground, {
+      duration: 0.4,
+      opacity: 0,
+    });
+  };
+
+  const handleHover = (e) => {
+    gsap.to(e.target, {
+      duration: 0.3,
+      y: 3,
+      skewX: 4,
+      ease: "power3.inOut",
+    });
+  };
+
+  const handleHoverExit = (e) => {
+    gsap.to(e.target, {
+      duration: 0.3,
+      y: -3,
+      skewX: 0,
+      ease: "power3.inOut",
+    });
+  };
+
+  return (
+    <div ref={(el) => (menu = el)} className="hamburger-menu">
+      <div
+        ref={(el) => (revealMenuBackground = el)}
+        className="menu-secondary-background-color"
+      ></div>
+      <div ref={(el) => (revealMenu = el)} className="menu-layer">
+        <div
+          ref={(el) => (cityBackground = el)}
+          className="menu-city-background"
+        ></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line1 = el)}
+                      to="/opportunities"
+                    >
+                      Opportunities
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line2 = el)}
+                      to="/solutions"
+                    >
+                      Solutions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line3 = el)}
+                      to="/contact"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <div ref={(el) => (info = el)} className="info">
+                <h3>Our Promise</h3>
+                <p>
+                  Contrary to popular belief, Lorem Ipsum is not simply random
+                  text. It has roots in a piece of classical Latin literature
+                  from 45 BC, making it over 2000 years old.
+                </p>
+              </div>
+              <div className="locations">
+                Locations:
+                {cities.map((city) => (
+                  <span
+                    key={city.name}
+                    onMouseEnter={() => handleCity(city.image)}
+                    onMouseOut={handleCityRemove}
+                  >
+                    {city.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hamburger;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import Hamburger from './Hamburger';
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import Hamburger from "./Hamburger";
 
-const Header = () => {
-    return (
-        <header>Logo
-            <Hamburger />
-        </header>
-    )
-}
+const Header = ({ history }) => {
+  //state for menu
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
 
-export default Header;
+  //state for disabled button
+  const [disabled, setDisabled] = useState(false);
+
+  //UseEffect for page changes
+  useEffect(() => {
+    //Listen for page changes
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  });
+
+  //function for opening ang closing the menu
+  const hanleMenu = () => {
+    disableMenu();
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+      //console.log(1)
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+      //console.log(2)
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+      //console.log(3)
+    }
+  };
+
+  //function that determine if the button should be disabled or not
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">HAMBRG.</Link>
+            </div>
+            <div className="menu">
+              <button disabled={disabled} onClick={hanleMenu}>
+                Menu
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Hamburger state={state} />
+    </header>
+  );
+};
+
+export default withRouter(Header);
